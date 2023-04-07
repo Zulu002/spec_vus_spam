9aedf321b7da5eb20f89cd2f7cdd2e9c25ef823e
Foo0s <oraproavp@list.ru>
2023-04-07 22:08:27 +0300

    Заполнение БД. Тест

---
+++ database/data.py
@@ -0,0 +1,12 @@
+from db import database
+
+data = [[1, "Владимир", "Как дела"],
+        [2, "Дмитрий", "Привет всем!!"],
+        [3, "Алексей", "Аллло"],
+        [1, "Владимир", "АЛЛО"]]
+
+#database.create_data_base() #Создание БД.
+#for i in data:
+    #database.write_in_data_base(i[0], i[1], i[-1])
+
+#database.delete_message(1) #Удаление значений.
--- database/db.py
+++ database/db.py
@@ -7,29 +7,30 @@ class database:
         """Создание БД."""
         with open("database.json", "w", encoding='UTF-8') as f:
             data = {"user_username": []}
-            json.dump(data, f, indent=3)
+            json.dump(data, f, indent=3, ensure_ascii=False)
 
     @staticmethod
     def write_in_data_base(user_id, name, text):
-        with open("database.json") as f2:
-            data = {"people": {"user_id": user_id, "name": name, "message": text}}
+        with open("database.json", encoding='UTF-8') as f2:
+            data = {"people": {"user_id": int(user_id), "name": str(name), "message": str(text)}}
             a = json.load(f2)
             a["user_username"].append(data)
-        with open("database.json", "w", encoding='UTF_8') as f3:
-            json.dump(a, f3, indent=3)
+        with open("database.json", "w", encoding='UTF-8') as f3:
+            json.dump(a, f3, indent=3, ensure_ascii=False)
             
 
     @staticmethod
     def delete_message(user_id):
-        with open("database.json") as file:
+        with open("database.json", encoding="UTF-8") as file:
             f = json.load(file)
 
             for i in f["user_username"]:
                 if user_id == i["people"]["user_id"]:
                     f["user_username"].remove(i)
+                    continue
         
-        with open("database.json", "w") as f4:
-            json.dump(f, f4, indent=4)
+        with open("database.json", "w", encoding='UTF-8') as f4:
+            json.dump(f, f4, indent=4, ensure_ascii=False)
 
 
 
